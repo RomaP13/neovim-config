8dfea64c882cacb32145efd9a450748b793702fc
Roman Pomazan <romapomazan1310@gmail.com>
2024-04-11 18:49:11 +0300

    Refactored config structure

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,11 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  config = function()
+    local config = require("lualine")
+    config.setup({
+      options = {
+        tme = "palenight"
+      }
+    })
+  end
+}
---
+++ lua/vim-options.lua
@@ -0,0 +1,11 @@
+vim.g.mapleader = " "
+
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set shiftwidth=2")
+
+vim.cmd("set clipboard+=unnamedplus")
+
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
