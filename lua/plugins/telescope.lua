ecaf34ec97f7e06e8961f26d59c86f6febb79db2
Roman Pomazan <romapomazan1310@gmail.com>
2024-04-12 18:53:02 +0300

    Added none-ls, telescope-ui-select and lspconfig with mason stuff

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,30 @@
 return {
-  "nvim-telescope/telescope.nvim", tag = "0.1.6",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  config = function()
-    local builtin = require("telescope.builtin")
-    local map = vim.keymap.set
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			local map = vim.keymap.set
 
-    map("n", "<leader>ff", builtin.find_files, {})
-    map("n", "<leader>fg", builtin.live_grep, {})
-    map("n", "<leader>fb", builtin.buffers, {})
-    map("n", "<leader>fh", builtin.help_tags, {})
-  end
+			map("n", "<leader>ff", builtin.find_files, {})
+			map("n", "<leader>fg", builtin.live_grep, {})
+			map("n", "<leader>fb", builtin.buffers, {})
+			map("n", "<leader>fh", builtin.help_tags, {})
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			local config = require("telescope")
+			config.setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			require("telescope").load_extension("ui-select")
+		end,
+	},
 }
