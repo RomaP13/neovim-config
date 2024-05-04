0b47a2798d57ae628fcc8c1c66066e5e9820f148
Roman Pomazan <romapomazan1310@gmail.com>
2024-05-04 20:01:12 +0300

    Added best mappings and options

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -6,6 +6,35 @@ vim.cmd("set shiftwidth=2")
 
 vim.cmd("set clipboard+=unnamedplus")
 
+vim.opt.mouse = "a"
+-- Make line numbers default
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.breakindent = true
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keep signcolumn on by default
+vim.opt.signcolumn = "yes"
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+vim.opt.scrolloff = 20
+
+vim.g.have_nerd_font = true
+
 -- disable netrw at the very start of your init.lua
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
