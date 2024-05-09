3b6d1f6bd2d4c8bc8dbd0b6631f5cd476f548d2d
Roman Pomazan <romapomazan1310@gmail.com>
2024-05-09 20:18:24 +0300

    Replaced lsp config with coc

---
+++ lua/plugins/coc.lua
@@ -0,0 +1,44 @@
+return {
+  {
+    "neoclide/coc.nvim",
+    branch = "release",
+    config = function()
+      local map = vim.keymap.set
+
+      -- Use <c-space> to trigger completion
+      map("i", "<C-Space>", "coc#refresh()", {silent = true, expr = true})
+
+      -- Diasnostics
+      map("n", "[g", "<Plug>(coc-diagnostic-prev)", {silent = true})
+      map("n", "]g", "<Plug>(coc-diagnostic-next)", {silent = true})
+
+      -- GoTo code navigation
+      map("n", "gd", "<Plug>(coc-definition)", {silent = true})
+      map("n", "gy", "<Plug>(coc-type-definition)", {silent = true})
+      map("n", "gi", "<Plug>(coc-implementation)", {silent = true})
+      map("n", "gr", "<Plug>(coc-references)", {silent = true})
+
+      -- Use K to show documentation in preview window
+      function _G.show_docs()
+          local cw = vim.fn.expand('<cword>')
+          if vim.fn.index({'vim', 'help'}, vim.bo.filetype) >= 0 then
+              vim.api.nvim_command('h ' .. cw)
+          elseif vim.api.nvim_eval('coc#rpc#ready()') then
+              vim.fn.CocActionAsync('doHover')
+          else
+              vim.api.nvim_command('!' .. vim.o.keywordprg .. ' ' .. cw)
+          end
+      end
+      map("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
+
+      -- Symbol renaming
+      map("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
+
+      map("i", "<CR>", "coc#pum#visible() ? '\\<C-y>' : '\\<CR>'", {silent = true, expr = true})
+    end,
+  },
+  {
+    "yaegassy/coc-pylsp",
+    build = "yarn install --frozen-lockfile"
+  },
+}
