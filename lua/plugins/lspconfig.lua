434405d0beb7162ca8d8ebcde98f04aeeeb9cf93
Roman Pomazan <romapomazan1310@gmail.com>
2024-07-08 17:37:22 +0300

    Added config for pylsp

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -52,6 +52,26 @@ return {
         capabilities = capabilities,
       })
       lspconfig.pylsp.setup({
+        settings = {
+          pylsp = {
+            plugins = {
+              -- formatter options
+              black = { enabled = true },
+              autopep8 = { enabled = false },
+              yapf = { enabled = false },
+              -- linter options
+              pylint = { enabled = true, executable = "pylint" },
+              pyflakes = { enabled = false },
+              pycodestyle = { enabled = false },
+              -- type checker
+              pylsp_mypy = { enabled = true },
+              -- auto-completion options
+              jedi_completion = { fuzzy = true },
+              -- import sorting
+              pyls_isort = { enabled = true },
+            },
+          },
+        },
         capabilities = capabilities,
       })
       lspconfig.bashls.setup({
