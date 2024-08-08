3860dc199cf50a6c719b6e8c90f088a44024ae6d
Roman Pomazan <romapomazan1310@gmail.com>
2024-08-08 18:37:18 +0300

    feat(theme): enhance catppuccin config with custom highlights

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -6,8 +6,18 @@ return {
   config = function()
     local config = require("catppuccin")
     config.setup({
+      flavour = "macchiato",
       transparent_background = true,
+
+      custom_highlights = function(colors)
+        return {
+          LineNr = { fg = colors.rosewater },
+          CursorLineNr = { fg = colors.green },
+        }
+      end,
+
     })
+
     -- setup must be called before loading
     vim.cmd.colorscheme("catppuccin")
   end,
