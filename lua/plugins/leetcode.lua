bcc219df6418664941bea55b2f1b62efdb499e96
Roman Pomazan <romapomazan1310@gmail.com>
2024-08-15 18:02:21 +0300

    feat: add LeetCode plugin

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,21 @@
+return {
+  "kawre/leetcode.nvim",
+  cmd = "Leet",
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    "rcarriga/nvim-notify",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    lang = "python3",
+    injector = {
+      ["python3"] = {
+        before = true
+      },
+    },
+  },
+}
