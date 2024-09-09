d79e081895f26db84a54aeea12376752a00ac7a8
Roman Pomazan <romapomazan1310@gmail.com>
2024-09-09 20:16:23 +0300

    feat: add some autocmds

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,8 +1,53 @@
--- Highlight when yanking (copying) text
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+local fn = vim.fn
+
+vim.api.nvim_create_augroup("bufcheck", { clear = true })
+
+-- Reload config file on change
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group   = "bufcheck",
+  pattern = vim.env.MYVIMRC,
+  command = "silent source %"
+})
+
+-- Highlight yanks
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group    = "bufcheck",
+  pattern  = "*",
+  callback = function() vim.highlight.on_yank { timeout = 500 } end
+})
+
+-- Sort imports and format code on save for Python files
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group    = "CocGroup",
+  pattern  = "*.py",
   callback = function()
-    vim.highlight.on_yank({higroup='IncSearch', timeout=150})
+    -- Sort imports
+    -- fn.CocAction("runCommand", "python.sortImports")
+    fn.CocAction("runCommand", "editor.action.organizeImport")
+    -- Format code
+    fn.CocAction("format")
   end,
+})
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group    = "CocGroup",
+  pattern  = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+  callback = function()
+    -- Sort imports
+    fn.CocAction("runCommand", "editor.action.organizeImport")
+    -- Format code
+    fn.CocAction("format")
+  end,
+})
+
+-- Return to last edit position when opening files
+vim.api.nvim_create_autocmd("BufReadPost", {
+  group    = "bufcheck",
+  pattern  = "*",
+  callback = function()
+    if fn.line("'\"") > 0 and fn.line("'\"") <= fn.line("$") then
+      fn.setpos(".", fn.getpos("'\""))
+      vim.cmd("silent! foldopen")
+    end
+  end
 })
