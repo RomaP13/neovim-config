db72f8fa65f53581c4f2712abc55f00ce54b743e
Roman Pomazan <romapomazan1310@gmail.com>
2024-09-11 13:47:14 +0300

    fix(lualine): fix macro recording 'q'
    Macro recording (q) doesn't work with noice. Fixed by showing recording
    messages in statusline with lualine.
    
    References:
    
    - https://github.com/folke/noice.nvim/issues/225
    - https://github.com/folke/noice.nvim/wiki/Configuration-Recipes#show-recording-messages

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -6,6 +6,15 @@ return {
       options = {
         theme = "catppuccin",
       },
+      sections = {
+        lualine_x = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+            color = { fg = "#ff9e64" },
+          }
+        },
+      },
     })
   end,
 }
