3f3269287fe6ab890461d3deeb43da7ea5da813c
Roman Pomazan <romapomazan1310@gmail.com>
2024-10-29 18:02:55 +0200

    feat(telescope): add new mapping
    - Added a new mapping for deleting a buffer from the list.
    - Updated other telescope mappings by setting 'silent' to them.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,7 @@ return {
 
       local config = require("telescope")
       local builtin = require("telescope.builtin")
+      local action_state = require("telescope.actions.state")
 
       config.setup({
         extensions = {
@@ -35,16 +36,38 @@ return {
         config.load_extension(ext)
       end
 
-      map("n", "<leader>ff", builtin.find_files, {})
-      map("n", "<leader>fg", builtin.live_grep, {})
-      map("n", "<leader>fb", builtin.buffers, {})
-      map("n", "<leader>fh", builtin.help_tags, {})
-      map("n", "<leader>fw", builtin.grep_string, {})
-      map("n", "<leader>fr", builtin.resume, {})
-      map("n", "<leader>gt", builtin.git_status, {})
-      map("n", "<leader>gc", builtin.git_commits, {})
+      map("n", "<leader>ff", builtin.find_files, { silent = true })
+      map("n", "<leader>fg", builtin.live_grep, { silent = true })
+      map("n", "<leader>fh", builtin.help_tags, { silent = true })
+      map("n", "<leader>fw", builtin.grep_string, { silent = true })
+      map("n", "<leader>fr", builtin.resume, { silent = true })
+      map("n", "<leader>gt", builtin.git_status, { silent = true })
+      map("n", "<leader>gc", builtin.git_commits, { silent = true })
 
-      map("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
+      map("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { silent = true })
+
+      -- Open buffers with Telescope. Press Ctrl-r to delete buffer from the list
+      map("n", "<leader>fb", function()
+        builtin.buffers({
+          initial_mode = "insert",
+          attach_mappings = function(prompt_bufnr, mapp)
+            local delete_buf = function()
+              local current_picker = action_state.get_current_picker(prompt_bufnr)
+              current_picker:delete_selection(function(selection)
+                vim.api.nvim_buf_delete(selection.bufnr, { force = true })
+              end)
+            end
+
+            mapp("i", "<C-r>", delete_buf)
+
+            return true
+          end
+        }, {
+          sort_lastused = true,
+          sort_mru = true,
+          theme = "dropdown"
+        })
+      end, { silent = true })
     end,
   },
 }
