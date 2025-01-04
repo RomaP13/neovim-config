3ca42646493eba2d88caf476d8034662bed585d5
Roman Pomazan <romapomazan1310@gmail.com>
2025-01-04 11:35:32 +0200

    feat: introduce a buffer-local variable that toggles formatting on save
    Usage:
    - Run `:ToggleFormatOnSave` in the desired buffer to enable or disable
    formatting on save for that buffer.
    - By default, format_on_save is enabled unless explicitly toggled off.

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -16,30 +16,6 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   callback = function() vim.highlight.on_yank { timeout = 500 } end
 })
 
--- Sort imports and format code on save for Python files
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group    = "CocGroup",
-  pattern  = "*.py",
-  callback = function()
-    -- Sort imports
-    -- fn.CocAction("runCommand", "python.sortImports")
-    fn.CocAction("runCommand", "editor.action.organizeImport")
-    -- Format code
-    fn.CocAction("format")
-  end,
-})
-
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group    = "CocGroup",
-  pattern  = { "*.js", "*.jsx", "*.ts", "*.tsx" },
-  callback = function()
-    -- Sort imports
-    fn.CocAction("runCommand", "editor.action.organizeImport")
-    -- Format code
-    fn.CocAction("format")
-  end,
-})
-
 -- Return to last edit position when opening files
 vim.api.nvim_create_autocmd("BufReadPost", {
   group    = "bufcheck",
@@ -50,4 +26,54 @@ vim.api.nvim_create_autocmd("BufReadPost", {
       vim.cmd("silent! foldopen")
     end
   end
+})
+
+-- Define a function to check if formatting should run
+local function should_format()
+  return vim.b.format_on_save ~= false
+end
+
+-- Python: Sort imports and format code on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group    = "CocGroup",
+  pattern  = "*.py",
+  callback = function()
+    if should_format() then
+      -- Sort imports
+      fn.CocAction("runCommand", "editor.action.organizeImport")
+      -- Format code
+      fn.CocAction("format")
+    end
+  end,
+})
+
+-- JavaScript/TypeScript: Sort imports and format code on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group    = "CocGroup",
+  pattern  = { "*.js", "*.jsx", "*.ts", "*.tsx" },
+  callback = function()
+    if should_format() then
+      -- Sort imports
+      fn.CocAction("runCommand", "editor.action.organizeImport")
+      -- Format code
+      fn.CocAction("format")
+    end
+  end,
+})
+
+-- Add commands to toggle formatting
+vim.api.nvim_create_user_command("ToggleFormatOnSave", function()
+  vim.b.format_on_save = not vim.b.format_on_save
+  print("Format on save:", vim.b.format_on_save)
+end, {})
+
+-- Set default value
+vim.api.nvim_create_autocmd("BufEnter", {
+  group    = "bufcheck",
+  pattern  = "*",
+  callback = function()
+    if vim.b.format_on_save == nil then
+      vim.b.format_on_save = true
+    end
+  end,
 })
