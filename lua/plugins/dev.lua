3ed658e7b62f403e2962e2252faa69e141b0ab67
Roman Pomazan <romapomazan1310@gmail.com>
2025-02-18 16:52:39 +0200

    feat: add file for loading dev plugins

---
+++ lua/plugins/dev.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    dir = "~/Projects/present.nvim",
+    config = function()
+      require("present")
+    end,
+  },
+  {
+    dir = "~/Projects/python-tools.nvim",
+    config = function()
+      return "python-tools"
+    end,
+  },
+}
