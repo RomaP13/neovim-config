ed1329417b115cccccfd1ac13a5167a4975ec645
Roman Pomazan <romapomazan1310@gmail.com>
2025-02-25 20:25:46 +0200

    chore(lualine): update lualine for better viewing some stuff

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,21 +2,39 @@ return {
   "nvim-lualine/lualine.nvim",
   config = function()
     local config = require("lualine")
+
+    local function show_codeium_status()
+      return "AI: " .. vim.fn["codeium#GetStatusString"]()
+    end
+
+    local function macro_recording()
+      local reg = vim.fn.reg_recording()
+      if reg == "" then
+        return ""
+      else
+        return "Recording @" .. reg
+      end
+    end
+
     config.setup({
       options = {
         theme = "catppuccin",
       },
-      -- TODO: find a way to fix this or just replace it with another statusline plugin
-      -- This secion fixes macros issue, but this causing missing borders for signature help
-      -- sections = {
-      --   lualine_x = {
-      --     {
-      --       require("noice").api.statusline.mode.get,
-      --       cond = require("noice").api.statusline.mode.has,
-      --       color = { fg = "#ff9e64" },
-      --     }
-      --   },
-      -- },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "filename" },
+        lualine_x = { show_codeium_status, "encoding", "fileformat", "filetype" },
+        lualine_y = { "selectioncount", "searchcount", "progress" },
+        lualine_z = { macro_recording, "location" },
+      },
     })
+
+    -- Remove searchcount from command line
+    vim.opt.shortmess:append("S")
+    -- Remove "recording @a" when recording a macro from command line
+    vim.opt.shortmess:append("q")
+    -- Remove the display of the partially typed commands from command line
+    vim.opt.showcmd = false
   end,
 }
