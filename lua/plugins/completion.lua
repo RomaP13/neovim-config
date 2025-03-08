74336cd49fc3f60cbdfafb3684023311d9b65df5
Roman Pomazan <romapomazan1310@gmail.com>
2025-03-08 18:09:34 +0200

    feat(blink): add blink compat plugin for supporting obsidian.nvim

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,66 +1,93 @@
 return {
-  "saghen/blink.cmp",
+  {
+    "saghen/blink.compat",
+    -- use the latest release, via version = '*', if you also use the latest release for blink.cmp
+    version = "*",
+    -- lazy.nvim will automatically load the plugin when it's required by blink.cmp
+    lazy = true,
+    -- make sure to set opts so that lazy.nvim calls blink.compat's setup
+    opts = {},
+  },
+  {
+    "saghen/blink.cmp",
 
-  -- TODO: add blink compat plugin
+    -- use a release tag to download pre-built binaries
+    version = "*",
 
-  -- use a release tag to download pre-built binaries
-  version = "*",
+    opts = {
+      -- See the full "keymap" documentation for information on defining your own keymap.
+      keymap = {
+        preset = "none",
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-d>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
 
-  opts = {
-    -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = {
-      preset = "none",
-      ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-      ["<C-d>"] = { "hide" },
-      ["<C-y>"] = { "select_and_accept" },
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback" },
+        ["<C-n>"] = { "select_next", "fallback" },
 
-      ["<Up>"] = { "select_prev", "fallback" },
-      ["<Down>"] = { "select_next", "fallback" },
-      ["<C-p>"] = { "select_prev", "fallback" },
-      ["<C-n>"] = { "select_next", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
 
-      ["<Tab>"] = { "snippet_forward", "fallback" },
-      ["<S-Tab>"] = { "snippet_backward", "fallback" },
-
-      ["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
-    },
-
-    completion = {
-      menu = {
-        border = "rounded",
+        ["<C-s>"] = { "show_signature", "hide_signature", "fallback" },
       },
-      documentation = {
-        auto_show = true,
-        auto_show_delay_ms = 500,
+
+      completion = {
+        menu = {
+          border = "rounded",
+        },
+        documentation = {
+          auto_show = true,
+          auto_show_delay_ms = 500,
+          window = {
+            border = "rounded",
+          },
+        },
+      },
+      signature = {
+        enabled = true,
         window = {
+          show_documentation = true,
           border = "rounded",
         },
       },
-    },
-    signature = {
-      enabled = true,
-      window = {
-        show_documentation = true,
-        border = "rounded",
+
+      appearance = {
+        -- Sets the fallback highlight groups to nvim-cmp"s highlight groups
+        -- Useful for when your theme doesn"t support blink.cmp
+        -- Will be removed in a future release
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = "normal",
+      },
+
+      -- TODO: add some snippets(LuaSnip I guess)
+      -- Check that they don't interfere my coding.
+      -- Also, find out about 'TODO' snippet. Maybe, I care to add dates for TODO.
+
+      -- Default list of enabled providers defined so that you can extend it
+      -- elsewhere in your config, without redefining it, due to `opts_extend`
+      sources = {
+        default = { "lsp", "path", "snippets", "buffer", "obsidian", "obsidian_new", "obsidian_tags" },
+        providers = {
+          obsidian = {
+            name = "obsidian",
+            module = "blink.compat.source",
+          },
+          obsidian_new = {
+            name = "obsidian_new",
+            module = "blink.compat.source",
+          },
+          obsidian_tags = {
+            name = "obsidian_tags",
+            module = "blink.compat.source",
+          },
+        },
       },
     },
-
-    appearance = {
-      -- Sets the fallback highlight groups to nvim-cmp"s highlight groups
-      -- Useful for when your theme doesn"t support blink.cmp
-      -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "normal",
-    },
-
-    -- Default list of enabled providers defined so that you can extend it
-    -- elsewhere in your config, without redefining it, due to `opts_extend`
-    sources = {
-      default = { "lsp", "path", "snippets", "buffer" },
-    },
+    opts_extend = { "sources.default" },
   },
-  opts_extend = { "sources.default" },
 }
