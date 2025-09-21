fa9d4b86473a6222f06aa04a0f2a6997844be6e2
Roman Pomazan <romapomazan1310@gmail.com>
2025-09-21 10:34:37 +0300

    fix(markdown): don't use render-markdown in LSP docs

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -4,5 +4,15 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
-  opts = {},
+  opts = {
+    overrides = {
+      buftype = {
+        nofile = {
+          render_modes = false, -- Disable all rendering in nofile buffers (LSP hovers)
+          padding = { highlight = "NormalFloat" }, -- Fallback to transparent float if needed
+          sign = { enabled = false }, -- Disable signs in hovers
+        },
+      },
+    },
+  },
 }
