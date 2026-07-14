518ff41b86b021554496feccb02851e670c58aa6
Roman Pomazan <romapomazan1310@gmail.com>
2026-07-14 11:11:07 +0300

    build(colorizer): switch to maintained fork

--- lua/plugins/ui/colorizer.lua
+++ lua/plugins/ui/colorizer.lua
@@ -1,10 +1,9 @@
 return {
-  "norcalli/nvim-colorizer.lua",
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
   opts = {
-    "css",
-    "javascript",
-    html = {
-      mode = "foreground",
+    parsers = {
+      css = true,
     },
   },
 }
