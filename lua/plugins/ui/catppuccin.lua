ceb8fbbbe2dafed55e7c353c1bd2f30d334b8caa
Roman Pomazan <romapomazan1310@gmail.com>
2026-07-14 13:50:16 +0300

    style(catppuccin): use opts instead of config

--- lua/plugins/ui/catppuccin.lua
+++ lua/plugins/ui/catppuccin.lua
@@ -1,58 +1,51 @@
 return {
   "catppuccin/nvim",
   lazy = false,
-  enabled = false,
   name = "catppuccin",
   priority = 1000,
-  config = function()
-    local config = require("catppuccin")
-    config.setup({
-      flavour = "macchiato",
-      transparent_background = true,
-      float = {
-        transparent = true,
-        solid = false,
+  opts = {
+    flavour = "macchiato",
+    transparent_background = true,
+    float = {
+      transparent = true,
+      solid = false,
+    },
+
+    custom_highlights = function(colors)
+      return {
+        LineNr = { fg = colors.rosewater },
+        CursorLineNr = { fg = colors.green },
+        Comment = { fg = "#808A9F" },
+
+        -- Custom highlights for blink-cmp borders
+        BlinkCmpMenuBorder = { fg = colors.blue }, -- Completion menu border
+        BlinkCmpDocBorder = { fg = colors.blue }, -- Documentation border
+        BlinkCmpSignatureHelpBorder = { fg = colors.blue }, -- Signature help border
+
+        -- Custom highlights for diffview
+        DiffAdd = { bg = "#A6DA95", fg = "#0E2239" },
+        DiffDelete = { bg = "#ED8796", fg = "#000000" },
+        DiffChange = { bg = "#EED49F", fg = "#000000" },
+        DiffText = { bg = "#B9C0F8", fg = "#0E0953" },
+      }
+    end,
+
+    integrations = {
+      blink_cmp = true,
+      diffview = true,
+      fidget = true,
+      fzf = true,
+      gitsigns = true,
+      markdown = true,
+      mason = true,
+      dap = true,
+      dap_ui = true,
+      treesitter = true,
+      ufo = true,
+      telescope = {
+        enabled = true,
       },
-
-      custom_highlights = function(colors)
-        return {
-          LineNr = { fg = colors.rosewater },
-          CursorLineNr = { fg = colors.green },
-          Comment = { fg = "#808A9F" },
-
-          -- Custom highlights for blink-cmp borders
-          BlinkCmpMenuBorder = { fg = colors.blue }, -- Completion menu border
-          BlinkCmpDocBorder = { fg = colors.blue }, -- Documentation border
-          BlinkCmpSignatureHelpBorder = { fg = colors.blue }, -- Signature help border
-
-          -- Custom highlights for diffview
-          DiffAdd = { bg = "#A6DA95", fg = "#0E2239" },
-          DiffDelete = { bg = "#ED8796", fg = "#000000" },
-          DiffChange = { bg = "#EED49F", fg = "#000000" },
-          DiffText = { bg = "#B9C0F8", fg = "#0E0953" },
-        }
-      end,
-
-      integrations = {
-        blink_cmp = true,
-        diffview = true,
-        fidget = true,
-        fzf = true,
-        gitsigns = true,
-        markdown = true,
-        mason = true,
-        dap = true,
-        dap_ui = true,
-        treesitter = true,
-        ufo = true,
-        telescope = {
-          enabled = true,
-        },
-        lsp_trouble = true,
-      },
-    })
-
-    -- setup must be called before loading
-    vim.cmd.colorscheme("catppuccin")
-  end,
+      lsp_trouble = true,
+    },
+  },
 }
