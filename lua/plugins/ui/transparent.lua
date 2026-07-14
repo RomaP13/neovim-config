23d1f262ba44a690732aa52e3c56ebca698442d5
Roman Pomazan <romapomazan1310@gmail.com>
2026-07-14 11:42:40 +0300

    style(transparent): use opts instead of config
    Also, disabled plugin by default.

--- lua/plugins/ui/transparent.lua
+++ lua/plugins/ui/transparent.lua
@@ -1,42 +1,39 @@
 return {
   "xiyaowong/transparent.nvim",
-  enabled = true,
-  config = function()
-    local config = require("transparent")
-    config.setup({
-      groups = {
-        "Normal",
-        "NormalNC",
-        "Comment",
-        "Constant",
-        "Special",
-        "Identifier",
-        "Statement",
-        "PreProc",
-        "Type",
-        "Underlined",
-        "Todo",
-        "String",
-        "Function",
-        "Conditional",
-        "Repeat",
-        "Operator",
-        "Structure",
-        "LineNr",
-        "NonText",
-        "SignColumn",
-        "CursorLine",
-        "CursorLineNr",
-        "StatusLine",
-        "StatusLineNC",
-        "EndOfBuffer",
-      },
-      extra_groups = {
-        "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
-        "NvimTreeNormal", -- NvimTree
-        "VertSplit", -- borders for some plugins
-        "Pmenu", -- Blink completion suggestions
-      },
-    })
-  end,
+  enabled = false,
+  opts = {
+    groups = {
+      "Normal",
+      "NormalNC",
+      "Comment",
+      "Constant",
+      "Special",
+      "Identifier",
+      "Statement",
+      "PreProc",
+      "Type",
+      "Underlined",
+      "Todo",
+      "String",
+      "Function",
+      "Conditional",
+      "Repeat",
+      "Operator",
+      "Structure",
+      "LineNr",
+      "NonText",
+      "SignColumn",
+      "CursorLine",
+      "CursorLineNr",
+      "StatusLine",
+      "StatusLineNC",
+      "EndOfBuffer",
+    },
+    extra_groups = {
+      "NormalFloat", -- plugins which have float panel such as Lazy, Mason, LspInfo
+      "NvimTreeNormal", -- NvimTree
+      "VertSplit", -- borders for some plugins
+      "Pmenu", -- Blink completion suggestions
+    },
+  },
 }
