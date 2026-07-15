7f85c1335de5225dfae8d5dd1e9e306f86ff724f
Roman Pomazan <romapomazan1310@gmail.com>
2026-07-15 09:56:19 +0300

    refactor(trouble): clean config

--- lua/plugins/lsp/trouble.lua
+++ lua/plugins/lsp/trouble.lua
@@ -1,32 +1,39 @@
 return {
   "folke/trouble.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
   cmd = "Trouble",
+
+  ---@module "trouble"
+  ---@type trouble.Config
   opts = {},
+
   keys = {
     {
       "<leader>aa",
-      ":Trouble diagnostics toggle<CR>",
-      desc = "Diagnostics (Trouble)",
+      "<cmd>Trouble diagnostics toggle<CR>",
+      desc = "Trouble: Diagnostics",
     },
     {
       "<leader>ab",
-      ":Trouble diagnostics toggle filter.buf=0<CR>",
-      desc = "Buffer Diagnostics (Trouble)",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+      desc = "Trouble: Buffer Diagnostics",
     },
     {
       "<leader>cs",
-      ":Trouble symbols toggle focus=false<CR>",
-      desc = "Symbols (Trouble)",
+      "<cmd>Trouble symbols toggle focus=false<CR>",
+      desc = "Trouble: Symbols",
     },
     {
       "<leader><CR>",
-      ":Trouble lsp toggle focus=false win.position=bottom<CR>",
-      desc = "LSP Definitions / references / ... (Trouble)",
+      "<cmd>Trouble lsp toggle focus=false win.position=bottom<CR>",
+      desc = "Trouble: LSP Definitions / references / ...",
     },
     {
       "<leader>td",
-      ":Trouble todo toggle focus=true<CR>",
-      desc = "TODOs (Trouble)",
+      "<cmd>Trouble todo toggle focus=true<CR>",
+      desc = "Trouble: TODOs",
     },
   },
 }
