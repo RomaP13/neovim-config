8df7571acfd6989b690f8e72cff0feb4c53c9270
Roman Pomazan <romapomazan1310@gmail.com>
2026-08-07 15:54:12 +0300

    feat(lightbulb): add lightbulb plugin
    Maybe temporarily...

---
+++ lua/plugins/lsp/lightbulb.lua
@@ -0,0 +1,17 @@
+return {
+  "kosayoda/nvim-lightbulb",
+
+  ---@module "nvim-lightbulb"
+  ---@type nvim-lightbulb.Config
+  opts = {
+    sign = {
+      enabled = false,
+    },
+    virtual_text = {
+      enabled = true,
+    },
+    autocmd = {
+      enabled = true,
+    },
+  },
+}
